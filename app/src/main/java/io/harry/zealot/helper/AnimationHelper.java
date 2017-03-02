f6e2f6eb9cc5c8e0e3ac3aea7f368cc745dc3d8e
Harry Yang <zillaci@gmail.com>
2017-03-02 14:55:03 +0900

    add method to get value increasing animation at AnimationHelper.

--- app/src/main/java/io/harry/zealot/helper/AnimationHelper.java
+++ app/src/main/java/io/harry/zealot/helper/AnimationHelper.java
@@ -1,11 +1,14 @@
 package io.harry.zealot.helper;
 
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 
 public class AnimationHelper {
+    private static final int FROM_VALUE = 0;
+
     //TODO: not using this class from anywhere.
     //TODO: 안쓰는 애니메이션도 정리할 것
     private final Context context;
@@ -20,5 +23,13 @@ public class AnimationHelper {
 
     public void startAnimation(View view, Animation animation) {
         view.startAnimation(animation);
+    }
+
+    public ValueAnimator getValueIncreaseAnimator(int toValue, int duration) {
+        ValueAnimator valueAnimator = new ValueAnimator();
+        valueAnimator.setDuration(duration);
+        valueAnimator.setObjectValues(FROM_VALUE, toValue);
+
+        return valueAnimator;
     }
 }
--- app/src/test/java/io/harry/zealot/helper/AnimationHelperTest.java
+++ app/src/test/java/io/harry/zealot/helper/AnimationHelperTest.java
@@ -1,24 +1,54 @@
 package io.harry.zealot.helper;
 
+import android.animation.PropertyValuesHolder;
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.view.View;
 import android.view.animation.Animation;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
 
+import io.harry.zealot.BuildConfig;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
+@RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class)
 public class AnimationHelperTest {
+
+    private AnimationHelper subject;
+
+    @Before
+    public void setUp() throws Exception {
+        subject = new AnimationHelper(mock(Context.class));
+    }
+
     @Test
     public void startAnimation_startsAnimationOnPassedView() throws Exception {
-        AnimationHelper subject = new AnimationHelper(mock(Context.class));
-
         View mockView = mock(View.class);
         Animation mockAnimation = mock(Animation.class);
 
         subject.startAnimation(mockView, mockAnimation);
 
         verify(mockView).startAnimation(mockAnimation);
+    }
+
+    @Test
+    public void getValueIncreaseAnimation_returnsValueAnimatorWithValueRangeAndDuration() throws Exception {
+        ValueAnimator valueIncreaseAnimator = subject.getValueIncreaseAnimator(67, 2000);
+
+        assertThat(valueIncreaseAnimator.getDuration()).isEqualTo(2000);
+
+        PropertyValuesHolder[] values = valueIncreaseAnimator.getValues();
+
+        assertThat(valueIncreaseAnimator.getAnimatedValue()).isEqualTo(null);
+        //this is how animate values are stored
+        assertThat(values[0].toString()).isEqualTo(":  0  67  ");
     }
 }
