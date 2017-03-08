6b29d708043911db1d53a96b0ecc084f8975f04f
Harry Yang <zillaci@gmail.com>
2017-03-08 15:42:18 +0900

    replace app_icon with the new one. delete unused layout.

--- app/src/main/java/io/harry/zealot/helper/AnimationHelper.java
+++ app/src/main/java/io/harry/zealot/helper/AnimationHelper.java
@@ -9,8 +9,6 @@ import android.view.animation.AnimationUtils;
 public class AnimationHelper {
     private static final int FROM_VALUE = 0;
 
-    //TODO: not using this class from anywhere.
-    //TODO: 안쓰는 애니메이션도 정리할 것
     private final Context context;
 
     public AnimationHelper(Context context) {
